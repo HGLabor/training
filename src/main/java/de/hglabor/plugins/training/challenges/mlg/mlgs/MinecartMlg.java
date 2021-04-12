40fbab043a7752f93798fac738316adafb8c216a
Krxwallo <ken.grill@gmx.de>
2021-04-12 14:05:04 +0200

    Added minecart mlg

--- src/main/java/de/hglabor/plugins/training/challenges/mlg/mlgs/MinecartMlg.java
+++ src/main/java/de/hglabor/plugins/training/challenges/mlg/mlgs/MinecartMlg.java
@@ -12,7 +12,7 @@ import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPlaceEvent;
@@ -20,15 +20,16 @@ import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class BoatMlg extends Mlg {
+public class MinecartMlg extends Mlg {
     private final List<ItemStack> mlgItems = new ArrayList<>();
 
-    public BoatMlg(String name, ChatColor color, Class<? extends Entity> type) {
-        super(name, color, type, Material.QUARTZ_BLOCK, Material.GOLD_BLOCK);
-        this.addMlgMaterial(Material.OAK_BOAT);
+    public MinecartMlg(String name, ChatColor color, Class<? extends Entity> type) {
+        super(name, color, type, Material.QUARTZ_BLOCK, new Material[]{Material.STONE, Material.RAIL});
+        this.addMlgMaterial(Material.MINECART);
     }
 
     private void addMlgMaterial(Material material) {
@@ -36,8 +37,8 @@ public class BoatMlg extends Mlg {
     }
 
     @EventHandler
-    public void onPlayerPlaceBoat(@SuppressWarnings("deprecation") /* api works for this usage */ EntityPlaceEvent event) {
-        if (!(event.getEntity() instanceof Boat)) return;
+    public void onPlayerPlaceMinecart(@SuppressWarnings("deprecation") /* api works for this usage */ EntityPlaceEvent event) {
+        if (!(event.getEntity() instanceof Minecart)) return;
         Player player = event.getPlayer();
         if (player == null) return;
         if (!(isInChallenge(player))) {
@@ -47,19 +48,19 @@ public class BoatMlg extends Mlg {
 
         User user = UserList.INSTANCE.getUser(player);
         if (!user.getChallengeInfoOrDefault(this, false)) {
-            if (event.getBlock().getType() != this.bottomMaterial) {
+            if (Arrays.stream(this.bottomMaterials).noneMatch((m -> m.equals(event.getBlock().getType())))) {
                 event.setCancelled(true);
                 return;
             }
-            // Remove boat after 1 second (20 ticks)
+            // Remove minecart after 1 second (20 ticks)
             Bukkit.getScheduler().runTaskLater(Training.getInstance(), () -> event.getEntity().remove(), 20L);
         }
     }
 
     @EventHandler
-    public void onPlayerEnterBoat(VehicleEnterEvent event) {
+    public void onPlayerEnterMinecart(VehicleEnterEvent event) {
         if (!(event.getEntered() instanceof Player)) return;
-        if (!(event.getVehicle() instanceof Boat)) return;
+        if (!(event.getVehicle() instanceof Minecart)) return;
         Player player = (Player) event.getEntered();
         if (!(isInChallenge(player))) {
             event.setCancelled(true);
@@ -68,7 +69,7 @@ public class BoatMlg extends Mlg {
         User user = UserList.INSTANCE.getUser(player);
         if (!user.getChallengeInfoOrDefault(this, false)) {
             onComplete(player);
-            // Boat already gets removed in onPlayerPlaceBoat
+            // Minecart already gets removed in onPlayerPlaceBoat
         } else {
             event.setCancelled(true);
         }
