43a0c1cae95208ad5df0750df412ed8496c3c5b2
Krxwallo <ken.grill@gmx.de>
2021-04-28 15:19:02 +0200

    Fixed streak high score name

--- src/main/java/de/hglabor/plugins/training/challenges/mlg/scoreboard/MlgScoreboard.java
+++ src/main/java/de/hglabor/plugins/training/challenges/mlg/scoreboard/MlgScoreboard.java
@@ -16,7 +16,7 @@ public class MlgScoreboard {
         StreakPlayer streakPlayer = StreakPlayer.get(player);
         ScoreboardFactory.addEntry(player, Names.MLG_NAME, player.getMlgName(),  " MLG", 10);
         ScoreboardFactory.addEntry(player, Names.STREAK, "Streak: ", String.valueOf(streakPlayer.getStreak(player.getMlgName())), 9);
-        ScoreboardFactory.addEntry(player, Names.HIGH_SCORE, "High Score: ", String.valueOf(streakPlayer.getHighScore(player.getMlgName())), 8);
+        ScoreboardFactory.addEntry(player, Names.HIGH_SCORE, "Streak High Score: ", String.valueOf(streakPlayer.getHighScore(player.getMlgName())), 8);
     }
 
     public static void update(MlgPlayer player) {
@@ -28,6 +28,6 @@ public class MlgScoreboard {
         StreakPlayer streakPlayer = StreakPlayer.get(player);
         ScoreboardFactory.updateEntry(player, Names.MLG_NAME, player.getMlgName());
         ScoreboardFactory.updateEntry(player, Names.STREAK, "Streak: ", String.valueOf(streakPlayer.getStreak(player.getMlgName())));
-        ScoreboardFactory.updateEntry(player, Names.HIGH_SCORE, "High Score: ", String.valueOf(streakPlayer.getHighScore(player.getMlgName())));
+        ScoreboardFactory.updateEntry(player, Names.HIGH_SCORE, "Streak High Score: ", String.valueOf(streakPlayer.getHighScore(player.getMlgName())));
     }
 }
