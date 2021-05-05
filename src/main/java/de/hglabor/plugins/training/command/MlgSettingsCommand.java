a890d7fb17de7c6b451957893d815cb0362a4d50
Krxwallo <ken.grill@gmx.de>
2021-05-05 11:00:00 +0200

    des hab ich vergessen rauszumachen

--- src/main/java/de/hglabor/plugins/training/command/MlgSettingsCommand.java
+++ src/main/java/de/hglabor/plugins/training/command/MlgSettingsCommand.java
@@ -1,15 +1,13 @@
 package de.hglabor.plugins.training.command;
 
-import de.hglabor.plugins.training.Training;
 import de.hglabor.plugins.training.settings.MlgSettings;
 import dev.jorel.commandapi.CommandAPICommand;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.TraderLlama;
 
 public class MlgSettingsCommand {
     public MlgSettingsCommand() {
         new CommandAPICommand("mlgsettings")
                 .executesPlayer((player, objects) -> {
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(Training.getInstance(), () -> MlgSettings.get(player.getUniqueId()).openGui()); }).register();
+                    MlgSettings.get(player.getUniqueId()).openGui();
+                }).register();
     }
 }
