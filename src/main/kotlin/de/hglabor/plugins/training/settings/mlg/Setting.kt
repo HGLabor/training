27392d307d6d6d6538b7119bf8922d1b00f8f17c
Krxwallo <ken.grill@gmx.de>
2021-06-01 12:49:48 +0200

    Add default values

--- src/main/kotlin/de/hglabor/plugins/training/settings/mlg/Setting.kt
+++ src/main/kotlin/de/hglabor/plugins/training/settings/mlg/Setting.kt
@@ -5,24 +5,24 @@ import org.bukkit.entity.Player
 import java.util.*
 import java.io.Serializable as javaSer
 
-enum class Setting (val settingName: String, val icon: Material? = null, val headOwner: String? = null, var enabled: HashMap<UUID, Boolean> = HashMap()) : javaSer {
+enum class Setting (val settingName: String, val icon: Material? = null, val headOwner: String? = null, var enabled: HashMap<UUID, Boolean> = HashMap(), private val default: Boolean = true) : javaSer {
 
     JUMP_SNEAK_ELEVATOR("Jump/Sneak Elevator", Material.MAGENTA_GLAZED_TERRACOTTA),
     LEVITATOR_SHEEP("Levitator Sheep", headOwner = "Kolish"),
-//    TOP_BOTTOM_PHANTOMS("Top/Bottom Phantoms", Material.PHANTOM_MEMBRANE)
+    TOP_BOTTOM_PHANTOMS("Top/Bottom Phantoms", Material.PHANTOM_MEMBRANE)
 
     ;
 
     fun toggle(uuid: UUID) { enabled[uuid] = enabled[uuid]?.not() ?: false }
 
     // best fun name ever CHANGE MY MIND
-    fun setDefaultEnabledIfMissing(uuid: UUID, default: Boolean = false) {
+    fun setDefaultEnabledIfMissing(uuid: UUID, default: Boolean = this.default) {
         if (enabled[uuid] != null) return
         enabled[uuid] = default
     }
 
-    fun getEnabled(uuid: UUID) = enabled[uuid]!!
-    fun getEnabled(player: Player) = enabled[player.uniqueId]!!
+    fun getEnabled(uuid: UUID) = enabled[uuid] ?: default
+    fun getEnabled(player: Player) = enabled[player.uniqueId] ?: default
 
     fun getEnabledPlayers(): ArrayList<UUID> {
         val players = ArrayList<UUID>()
